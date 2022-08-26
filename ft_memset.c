7757c6f6773ec556a644dee3e3d40dd787f7df36
Thanaphat Khemniwat <tkhemniw@z2t14c4.42bangkok.com>
2022-08-26 13:34:49 +0700

    fix iterator of memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: tkhemniw <gt.khemniwat@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 23:37:44 by tkhemniw          #+#    #+#             */
-/*   Updated: 2022/08/25 23:52:06 by tkhemniw         ###   ########.fr       */
+/*   Updated: 2022/08/26 13:32:23 by tkhemniw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ void    *ft_memset(void *dest, int val, size_t len)
     /*if (!dest)
 		return (NULL);*/
 	while (len-- > 0)
-		*((unsigned char*)dest) = (unsigned char)val;
+		*((unsigned char*)dest--) = (unsigned char)val;
 	return (dest);
 }

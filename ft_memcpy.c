21abf72fd13a08650f8b53eeb0b9070cc4238e47
Gxmestk <gt.khemniwat@gmail.com>
2022-09-01 04:12:29 +0700

    make crash

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -17,11 +17,11 @@ void	*ft_memcpy(void *dest, const void *src, size_t len)
 	char		*p_dest;
 	const char	*p_src;
 
-	if (!dest)
+	if (!dest)	
 		return (NULL);
 	p_dest = (char *)dest;
 	p_src = (char *)src;
-	while (len-- > 0 && (p_dest || p_src))
+	while (len-- > 0 )
 		*(p_dest++) = *(p_src++);
 	return (dest);
 }

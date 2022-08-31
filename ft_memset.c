e21d204b31ce8cc5df0956e43924ab90f51e4967
Gxmestk <gt.khemniwat@gmail.com>
2022-09-01 03:51:47 +0700

    make crash

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: tkhemniw <gt.khemniwat@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 23:37:44 by tkhemniw          #+#    #+#             */
-/*   Updated: 2022/08/27 16:48:21 by tkhemniw         ###   ########.fr       */
+/*   Updated: 2022/09/01 03:51:28 by tkhemniw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void	*ft_memset(void *dst, int val, size_t len)
 {
 	unsigned char	*p_dst;
 
-	if (!dst)
-		return (NULL);
 	p_dst = (unsigned char *)dst;
 	while (len-- > 0)
 		*(p_dst++) = (unsigned char)val;

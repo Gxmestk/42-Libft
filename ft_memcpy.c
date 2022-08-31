de3812dbb248910971b9d60aadc1e0d9ad4cf497
Gxmestk <gt.khemniwat@gmail.com>
2022-09-01 04:03:09 +0700

    make crash

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -17,9 +17,11 @@ void	*ft_memcpy(void *dest, const void *src, size_t len)
 	char		*p_dest;
 	const char	*p_src;
 
+	if (!dest)
+		return (NULL);
 	p_dest = (char *)dest;
 	p_src = (char *)src;
-	while (len-- > 0)
+	while (len-- > 0 && (p_dest || p_src))
 		*(p_dest++) = *(p_src++);
 	return (dest);
 }
---
+++ push.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+clear
+
+echo "------------------------------------------PUSH------------------------------------------"
+
+git add .
+git commit -m "make crash"
+git push
